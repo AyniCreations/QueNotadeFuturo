5b05a828800c0f8a6913bc8ddb735ebb508c1ab3
AyniCreations <93357249+AyniCreations@users.noreply.github.com>
2021-12-06 11:34:42 -0500

    Delete Backend directory

--- Backend/src/routes/usuarios.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { crearUsuario, getUsers } = require('../controllers/user.controller');
-
-router.route('/').get(getUsers).post(crearUsuario);
-
-module.exports = router;
